0c6e364d647035e6bd2b882e38d0fb1f459a0f76
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2022-09-16 13:26:11 +0200

    Update EXTI PINS

--- COTS/01_MCAL/EXTI/EXTI_private.h
+++ COTS/01_MCAL/EXTI/EXTI_private.h
@@ -30,7 +30,57 @@
 
 		#define  MCUCSR   *((volatile u8*)0x54)
 	
+ 	// Interrput Pins
 
+
+		// General Flag Pin used to Enable the external Interrupt
+	
+		#define GI      PIN7
+
+		/*
+			Interrupts Flag Pins used to execute the Interrupt when it is HIGH
+			Located in GIFR
+		*/
+		 
+		#define INTF0   PIN6
+		#define INTF1   PIN7
+		#define INTF2   PIN5
+
+
+		/*
+			Interrupts Flag Pins used to Enable the Interrupt when it is HIGH
+			Located in GICR
+		*/
+
+		#define INT0    PIN6
+		#define INT1    PIN7
+		#define INT2    PIN5
+
+
+		/*
+			Interrupt Sense Control Pins used to define the edge or level that enable the Interrupt number 1
+			Located in MCUCR
+		*/
+
+		#define ISC11   PIN3
+		#define ISC10   PIN2
+
+
+		/*
+			Interrupt Sense Control Pins used to define the edge or level that enable the Interrupt number 0
+			Located in MCUCR
+		*/
+
+		#define ISC01   PIN1
+		#define ISC00   PIN0
+
+
+		/*
+			Interrupt Sense Control Pin used to define the edge that enable the Interrupt number 2
+			Located in MCUCR
+		*/
+
+		#define ISC2    PIN6
 	
 		
 
